09181e13f2d598d179d9dca2278bd61d28ff624d
Mao Kaihe <maomao8672@outlook.com>
2026-02-04 23:41:21 +0800

    add

--- code/bpe.py
+++ code/bpe.py
@@ -8,6 +8,8 @@ for pre_token in pre_tokens:
 def encode_iterable(self, iterable: Iterable[str]) -> Iterable[int]:
     """
     内存高效的迭代编码器
+    分为块处理输入的可迭代对象，并逐个编码每个块
+    以减少内存使用
     """
     for chunk in iterable:
-        yield from self.encode(chunk)
+        yield from self.encode(chunk) 
